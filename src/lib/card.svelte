2eaf31ca9f62757bb6750b58db042a2326cd421f
Kulikovsky <80684329+klkvsky@users.noreply.github.com>
2023-02-04 17:40:09 +0500

    first commit

---
+++ src/lib/card.svelte
@@ -0,0 +1,11 @@
+<li class="bg-white border-[0.6px] border-neutral-300 p-[8px] flex-col rounded-[10px]">
+	<p class="font-black text-neutral-900 text-[16px]">Premier League</p>
+	<div
+		class="flex flex-row items-center h-[16px] w-full rounded-[4px] overflow-hidden mt-[12px] border-[0.6px] border-neutral-300]"
+	>
+		<div class="h-full bg-[#E20613] w-full" />
+		<div class="h-full bg-white w-full" />
+		<div class="h-full bg-yellow-500 w-full" />
+		<div class="h-full bg-orange-500 w-full" />
+	</div>
+</li>
---
+++ src/lib/footer.svelte
@@ -0,0 +1,26 @@
+<footer
+	class="bg-white w-full h-[46px] rounded-[10px] p-[12px] flex flex-row items-center justify-between mt-[16px] border-[0.6px] border-neutral-300 lg:max-w-[840px] lg:mx-auto"
+>
+	<a href="/" class="text-neutral-900 font-black text-[16px] flex flex-row items-center gap-[.5ch]">
+		football.colors <span class="hidden sm:block text-neutral-600 font-bold"
+			>built by <a
+				href="https://twitter.com/predragus"
+				target="_blank"
+				class="text-neutral-900 font-black"
+				rel="noreferrer"
+			>
+				Predrag
+			</a>
+			and
+			<a
+				href="https://twitter.com/klkvsky"
+				target="_blank"
+				class="text-neutral-900 font-black"
+				rel="noreferrer"
+			>
+				Klkvsky
+			</a></span
+		>
+	</a>
+	<a href="/aboutus" class="text-neutral-900 font-black text-[16px]"> About </a>
+</footer>
